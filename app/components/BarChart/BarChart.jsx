9ce00a42d0a63036038f3252fdd5c54e04dda821
Mats Byrkjeland <matsbyr@gmail.com>
2016-11-22 00:34:45 +0100

    Fetch real data

--- app/components/BarChart/BarChart.jsx
+++ app/components/BarChart/BarChart.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 
-const BarChart = () => {
+const BarChart = (props) => {
   const data = {
     labels: ['Abakus', 'Online'],
     datasets: [{
       label: 'Points',
-      data: [19, 12],
-      fillColor: 'rgba(220,0,0,1)',
-      strokeColor: 'rgba(220,0,0,1)',
-      borderWidth: 1,
+      data: props.data || [19, 12],
+      backgroundColor: [
+        '#b1281c',
+        '#004b80',
+      ],
+      borderColor: [
+        '#b1281c',
+        '#004b80',
+      ],
     }],
   };
 
-  const options = {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-        },
-      }],
-    },
-  };
+  const options = {};
 
-  return <Bar data={data} options={options} width="600" height="250" />;
+  return <Bar data={data} options={options} />;
 };
+
+BarChart.propTypes = {
+  data: React.PropTypes.arrayOf(
+    React.PropTypes.number,
+  ),
+};
+
 
 export default BarChart;
--- app/containers/BarGraphContainer/BarGraphContainer.jsx
+++ app/containers/BarGraphContainer/BarGraphContainer.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import BarChart from '../../components/BarChart/BarChart';
 
-const BarGraphContainer = () => {
-  const data = [];
+class BarGraphContainer extends React.Component {
 
-  return <BarChart data={data} color="blue" />;
-};
+  constructor() {
+    super();
+    this.state = {
+      abakusPoints: 0,
+      onlinePoints: 0,
+    };
+    this.fetchPoints();
+  }
+
+  fetchPoints() {
+    // Fetch Abakus' points
+    fetch('http://wcg-api.byrkje.land/teams/2TXCT2Z972').then(response => response.json())
+    .then((parsedResponse) => {
+      this.setState({
+        abakusPoints: parseInt(parsedResponse.teamStats.statisticsTotals.points, 10),
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+    // Fetch Online's points
+    fetch('http://wcg-api.byrkje.land/teams/3KWRNGFL72').then(response => response.json())
+    .then((parsedResponse) => {
+      this.setState({
+        onlinePoints: parseInt(parsedResponse.teamStats.statisticsTotals.points, 10),
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    return <BarChart data={[this.state.abakusPoints, this.state.onlinePoints]} />;
+  }
+
+}
 
 export default BarGraphContainer;
